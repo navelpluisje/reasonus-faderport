4010e99225af3410192674b475e505b270c1f007
Erwin Goossen <erwin@navelpluisje.nl>
2022-07-05 23:00:41 +0200

    Made the next step

--- out/reasonus-faderport/Scripts/always-on.lua
+++
@@ -1,9 +0,0 @@
-function main()
-  isNewValue, file, sec, cmd = reaper.get_action_context()
-  state = reaper.GetToggleCommandStateEx(sec, cmd);
-  reaper.SetToggleCommandState(sec, cmd, 1)
-  reaper.RefreshToolbar2(sec, cmd);
-end
-
-main()
-
