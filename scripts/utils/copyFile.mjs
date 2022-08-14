82b772b1386098ddf27008c2a4fad091ce16eb99
Erwin Goossen <erwin@navelpluisje.nl>
2022-08-14 16:04:47 +0200

    Started test with fxs

--- scripts/utils/copyFile.mjs
+++ scripts/utils/copyFile.mjs
@@ -5,6 +5,15 @@ import path from 'path';
 export const copyFile = (fromDir, toDir, fileName) => {
   console.log(chalk.green(`${new Date().toISOString()}: `) + chalk.yellow(fileName) + chalk.green(` changed.`));
 
+  const toFile = path.join(toDir, fileName);
+  const toFileDir = toFile.split(
+    '/',
+    toFile.split('/').length - 1
+  ).join('/');
+
+  if (!fs.existsSync(toFileDir)) {
+    fs.mkdirSync(toFileDir, { recursive: true })
+  }
   fs.copyFile(
     path.join(fromDir, fileName),
     path.join(toDir, fileName),
